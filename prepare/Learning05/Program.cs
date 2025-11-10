cb0722be30f1e98db447c1b83969b0f200112187
gagedorl <gagedorlando@gmail.com>
2025-11-10 09:38:04 -0700

    Did polymorphism prepare activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = [];
+
+        Square s = new("Red", 2);
+        shapeList.Add(s);
+
+        Rectangle r = new("Yellow", 2, 3);
+        shapeList.Add(r);
+
+        Circle c = new("Blue", 2);
+        shapeList.Add(c);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}");
+        }
     }
 }
